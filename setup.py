4c42c91306ca14de9bc29ea30368a88eb4f726ff
Capt. Dr. Guilherme Castelao <guilherme@castelao.net>
2013-05-09 11:51:22 +0200

    Minor version upgrade and some info in the NEWS.txt

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ Topic :: Scientific/Engineering
 Topic :: Software Development :: Libraries :: Python Modules
 """
 
-version = '0.4.0'
+version = '0.4.1'
 
 install_requires=[
     "numpy >= 1.1",

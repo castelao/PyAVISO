55efaf33bfcc26271fd954e2ec270ea3abd2e50d
Gui <guilherme@castelao.net>
2015-04-27 14:05:35 -0700

    Bugfix. Badly defined --products @ AVISO_download

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ Topic :: Scientific/Engineering
 Topic :: Software Development :: Libraries :: Python Modules
 """
 
-version = '0.9.0'
+version = '0.9.1'
 
 install_requires=[
     "numpy >= 1.1",
